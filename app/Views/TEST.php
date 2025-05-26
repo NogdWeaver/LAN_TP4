230836727008d9ebdf8cc7ed9c1c738f1dbaef84
Evan <elena.rouzo@proton.me>
2025-05-26 14:37:23 +0200

    Comments on TEST

--- app/Views/TEST.php
+++ app/Views/TEST.php
@@ -1,18 +1,27 @@
 <?php
 
-use App\Config\Database;
-use CodeIgniter\Database\Query;
-
+/**
+ *$db = db_connect(); est équivalent à  $db = \Config\Database::connect(); 
+ *  à mettre dans chaque fichier ou chaque class constructor pour la rendre disponible a toute la classe
+ * 
+ * $query = $db->query('YOUR QUERY HERE'); pour la requette
+ * 
+ *  Pour les résultats des requettes, l'utilisation du foreach ou encore la bone syntaxe $row, se référer a 
+ * https://www.codeigniter.com/user_guide/database/results.html#getresultarray
+ * 
+ * var_dump pour vérifier 
+ * 
+ */
 
 $db = db_connect();
 
 
 $query = $db->query("SELECT NOMjeu FROM jeu");
 
-foreach ($query->getResult('array')as $row){
+foreach ($query->getResultArray() as $row){
     echo $row['NOMjeu'];
-
 }
 
 var_dump($row);
 echo ("ok nice");
+
