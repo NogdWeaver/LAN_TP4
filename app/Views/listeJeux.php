5aad4c6dc434a2115458825928a9d38a47b09cbd
Evan <elena.rouzo@proton.me>
2025-05-28 14:52:53 +0200

    Game list without login is finished-ish, needs css, but back works ( jut list )

--- app/Views/listeJeux.php
+++ app/Views/listeJeux.php
@@ -4,26 +4,31 @@
     include("templates/header.php")
 ?>
 
+<h1>JEUX DU TOURNOI</h1>
 
-<?php if (empty($jeu)): ?>     
-    <div class ='divlistetest'>
-        Pas de Jeux!! :c 
+
+
+
+
+<?php foreach($jeu as $row): ?>
+
+    <div class="lsgridparent">
+
+        <div class="divls1">
+            <?= $row['NOMjeu'] ?>
+        </div>
+
+        <div class="divls2">
+            <?= $row['DESCjeu'] ?>
+        </div>
+        
+        <div class="divls3">
+           <img src=" <?= $row['IMGjeu'] ?>">
+        </div>
+
     </div>
-<?php else: ?>
-    <div class ='divlistetest'>  <?= var_dump($jeu) ?>  </div>
-    <?php foreach($jeu as $row): ?>
-            <div class='divlistetest'>
-            
-            <?= 
-            $row['NOMjeu'] 
-            ?>
-
-            </div>
 
     <?php endforeach; ?>
-<?php endif; ?>
-
-
 
 <?php
     include("templates/footer.php"); 
