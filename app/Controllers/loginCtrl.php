175854783c57a0a902def8f993d57a967bbd068a
Evan <elena.rouzo@proton.me>
2025-06-20 13:59:53 +0200

    Working on login

--- app/Controllers/loginCtrl.php
+++ app/Controllers/loginCtrl.php
@@ -4,6 +4,18 @@ use App\Controllers\BaseController;
 
 class login extends BaseController {
 
+private function loginuser(?object $user = null)  {
+    
+    $session = session();
+    $session->set([
+        'username' => isset($user) ? ($user['login']) . strtoupper($user['login']) :'admin',
+        'loggedIn' => true
+
+    ]); 
+    if ()
+    return view();
+}
+
 
 }
 
